31f6c350683ab3fb460377393460b927eed1526a
Dmitry Tiabutov <pardimitdir@gmail.com>
2025-03-12 22:44:55 +0500

    Updated methods 'requestChangeEmailLink'

--- src/main/java/ru/skillbox/social_network_authorization/configuration/SecurityConfiguration.java
+++ src/main/java/ru/skillbox/social_network_authorization/configuration/SecurityConfiguration.java
@@ -13,18 +13,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.skillbox.social_network_authorization.security.UserDetailsServiceImpl;
-import ru.skillbox.social_network_authorization.security.jwt.JwtAuthenticationEntryPoint;
-import ru.skillbox.social_network_authorization.security.jwt.JwtTokenFilter;
 
 @Configuration
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     private final UserDetailsServiceImpl userDetailsService;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-//    private final JwtTokenFilter jwtTokenFilter;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -41,18 +36,14 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auth) ->
+        http.authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .exceptionHandling(
-                        configurer -> configurer.authenticationEntryPoint(
-                                jwtAuthenticationEntryPoint))
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->
                         httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-//                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
