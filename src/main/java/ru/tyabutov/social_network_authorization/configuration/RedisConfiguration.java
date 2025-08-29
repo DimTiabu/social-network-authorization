67c6872e136390323c1a2668bf21a2ab5e2431e2
Dmitry Tiabutov <pardimitdir@gmail.com>
2025-08-29 09:34:08 +0500

    Updated Redis configuration

--- src/main/java/ru/tyabutov/social_network_authorization/configuration/RedisConfiguration.java
+++ src/main/java/ru/tyabutov/social_network_authorization/configuration/RedisConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.convert.KeyspaceConfiguration;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -27,16 +27,16 @@ public class RedisConfiguration {
     private Duration refreshTokenExpiration;
 
     @Bean
-    public JedisConnectionFactory jedisConnectionFactory(
-            RedisProperties redisProperties) {
+    public LettuceConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
         RedisStandaloneConfiguration configuration =
                 new RedisStandaloneConfiguration();
 
         configuration.setHostName(redisProperties.getHost());
         configuration.setPort(redisProperties.getPort());
 
-        return new JedisConnectionFactory(configuration);
+        return new LettuceConnectionFactory(configuration);
     }
+
 
     public class RefreshTokenKeyspaceConfiguration
             extends KeyspaceConfiguration {
